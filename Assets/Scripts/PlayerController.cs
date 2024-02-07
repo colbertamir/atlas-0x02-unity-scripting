d3eddfc06a7eab57b1c649d39765625dbb69caf8
colbertamir <colbertamir1516@gmail.com>
2024-02-07 14:51:03 -0600

    lets goo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 5f;
+	
+	void Start () 
+	{
+		// Use this for initialization
+	}
+	
+	void FixedUpdate () 
+	{
+		// Check for input and move the player accordingly
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Get input from A/D or left/right arrow keys
+        float moveVertical = Input.GetAxis("Vertical"); // Get input from W/S or up/down arrow keys
+
+        // Calculates the movement direction based on the input
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        // Normalize the movement vector to prevent faster diagonal movement
+        movement.Normalize();
+
+        // Move the player using Rigidbody physics
+        GetComponent<Rigidbody>().velocity = movement * speed;
+	}
+}
