f673f101203f4057b3454945a1d8cddc078f6ffd
colbertamir <colbertamir1516@gmail.com>
2024-02-07 20:19:16 -0600

    updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
-	public float speed = 5f;
-	
-	void Start () 
-	{
-		// Use this for initialization
-	}
-	
-	void FixedUpdate () 
-	{
-		// Check for input and move the player accordingly
-        float moveHorizontal = Input.GetAxis("Horizontal"); // Get input from A/D or left/right arrow keys
-        float moveVertical = Input.GetAxis("Vertical"); // Get input from W/S or up/down arrow keys
+    public float speed = 5f;
+    private Rigidbody rb;
 
-        // Calculates the movement direction based on the input
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+    void Start () 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate () 
+    {
+        // Check for input and move the player accordingly
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Gets input from A/D or left/right arrow keys
+        float moveVertical = Input.GetAxis("Vertical"); // Getw input from W/S or up/down arrow keys
 
-        // Normalize the movement vector to prevent faster diagonal movement
-        movement.Normalize();
+        // Calculates the movement based on the input
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        movement = movement.normalized * speed * Time.deltaTime;
 
-        // Move the player using Rigidbody physics
-        GetComponent<Rigidbody>().velocity = movement * speed;
-	}
+        // Move the player using physics
+        rb.MovePosition(transform.position + movement);
+    }
 }
